def52d0d70fc386ff3b2b685c75f82c63e89d0d4
Meir J Cohen <meir84@gmail.com>
2022-11-22 19:56:26 +0200

    added under construction header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 style={{ fontSize: "4.5rem", margin: "1.75rem", color: "hotpink" }}>
+          Under Construction!
+        </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
